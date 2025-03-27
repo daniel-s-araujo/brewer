c4f023b1c88b0362034dbbf506d58c92e12880db
Daniel Sena <daniels_araujo@outlook.com.br>
2025-03-27 12:40:30 -0300

    5.3. Baixando e configurando o layout no projeto

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -18,16 +18,22 @@ public class CervejasController {
 	public String novo(Cerveja cerveja) {
 		return "cerveja/CadastroCerveja";
 	}
-
+	
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
 	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
 			return novo(cerveja);
 		}
-
+		
+		// Salvar no banco de dados...
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
 		System.out.println(">>> sku: " + cerveja.getSku());
 		return "redirect:/cervejas/novo";
 	}
-
+	
+	@RequestMapping("/cervejas/cadastro")
+	public String cadastro() {
+		return "cerveja/cadastro-produto";
+	}
+	
 }
